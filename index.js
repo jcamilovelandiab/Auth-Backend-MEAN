fa1b02bfaffad164adc401a40174a9f48b11b112
Camilo <jcamilovelandiab@gmail.com>
2021-06-04 13:51:56 -0500

    Fixing routing bugs for the frontend.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path');
 require('dotenv').config();
 
 // Create the express server/application
@@ -18,6 +19,11 @@ app.use( cors() );
 app.use( express.json() );
 // Routes
 app.use('/api/auth', require('./routes/auth') );
+
+// Handle the other routes ( Angular )
+app.get( '*', ( req, res ) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 
 app.listen( process.env.PORT, () => {
     console.log(`Server running on port ${ process.env.PORT }`);
